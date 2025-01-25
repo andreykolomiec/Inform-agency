5ee381c4224f09d0cd8a660c96bf4a68f7b5b476
Andrew <akolomiec982@gmail.com>
2025-01-26 00:06:28 +0200

    Configure admin panel for Topic, Redactor and Newspaper models

--- newspapers/admin.py
+++ newspapers/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from newspapers.models import Newspaper, Topic, Redactor
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info", {"fields": ("years_of_experience",)}
+        ),
+    )
+    add_fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info", {"fields": ("years_of_experience",)}
+        ),
+    )
+    search_fields = ("years_of_experience",)
+    ordering = (
+        "years_of_experience",
+        "username",
+    )
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ("title", "content", "published_date", "topic")
+    search_fields = ("title",)
+    ordering = ("published_date",)
+    list_filter = ("topic",)
+
