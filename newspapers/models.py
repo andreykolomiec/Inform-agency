4439bcb15c5c7aaa82cc79e172ac0e9a7a6a697d
Andrew <akolomiec982@gmail.com>
2025-01-29 00:16:12 +0200

    Add get_absolute_url method to Redactor and Newspaper models

--- newspapers/models.py
+++ newspapers/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class Topic(models.Model):
@@ -22,11 +23,14 @@ class Redactor(AbstractUser):
     def __str__(self):
         return f"{self.username}: ({self.first_name} {self.last_name})"
 
+    def get_absolute_url(self):
+        return reverse("newspapers:redactor-detail", kwargs={"pk": self.pk})
+
 class Newspaper(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateField()
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name="newspapers")
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     publishers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
 
     class Meta:
@@ -34,3 +38,6 @@ class Newspaper(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("newspapers:newspaper-detail", args=[str(self.id)])
