9055fbe9341e4fe95e5732504614105281c6190c
Andrew <akolomiec982@gmail.com>
2025-01-29 00:31:08 +0200

    Add views with authentication protection: index, topic list, newspaper list, redactor list, and detail views. Applied pagination to all list views.

--- newspapers/views.py
+++ newspapers/views.py
@@ -1,3 +1,49 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from newspapers.models import Redactor, Newspaper, Topic
+
+
+@login_required
+def index(request):
+    """ View function for home page of the site. """
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        "num_topics": Topic.objects.count(),
+        "num_redactors": Redactor.objects.count(),
+        "num_newspapers": Newspaper.objects.count(),
+        "num_visits": num_visits + 1
+    }
+
+    return render(request, "newspapers/index.html", context=context)
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    queryset = Topic.objects.all().order_by("name")
+    paginate_by = 3
+
+
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
+    model = Newspaper
+    queryset = Newspaper.objects.select_related("topic")
+    paginate_by = 4
+
+
+class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Newspaper
+
+
+class RedactorListView(LoginRequiredMixin, generic.ListView):
+    model = Redactor
+    paginate_by = 3
+
+
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Redactor
+    queryset = Redactor.objects.prefetch_related("newspapers")
