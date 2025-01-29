523e8207eef3155b383f1b6782c97a43c059e30c
Andrew <akolomiec982@gmail.com>
2025-01-29 23:20:41 +0200

    feat: add CRUD URLs for topics, newspapers, and redactors

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from newspapers.views import (
     index,
     TopicListView,
+    TopicCreateView,
+    TopicUpdateView,
+    TopicDeleteView,
     NewspaperListView,
     NewspaperDetailView,
+    NewspaperCreateView,
     RedactorListView,
     RedactorDetailView
 )
@@ -13,9 +17,13 @@ from newspapers.views import (
 urlpatterns = [
     path("", index, name="index"),
     path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
+    path("topics/<int:pk>/update/", TopicUpdateView.as_view(), name="topic-update"),
+    path("topics/<int:pk>/delete/", TopicDeleteView.as_view(), name="topic-delete"),
 
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
     path("newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
 
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
     path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
