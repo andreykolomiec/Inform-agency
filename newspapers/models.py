1e52802db80e0838ffd374f470a2d39bc68c2efb
Andrew <akolomiec982@gmail.com>
2025-02-21 22:38:08 +0200

    Modify newspapers models: add ordering and absolute URL methods

--- newspapers/models.py
+++ newspapers/models.py
@@ -26,18 +26,27 @@ class Redactor(AbstractUser):
     def get_absolute_url(self):
         return reverse("newspapers:redactor-detail", kwargs={"pk": self.pk})
 
+
 class Newspaper(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateField()
-    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, blank=True, null=True)
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="newspapers"
+    )
 
     class Meta:
-        ordering = ("title",)
+        ordering = ("id",)
 
     def __str__(self):
-        return self.title
+        return (
+            f"{self.title}: ("
+            f"topic: {self.topic},"
+            f" publishers: {self.publishers},"
+            f" published_date: {self.published_date}"
+            f")"
+        )
 
     def get_absolute_url(self):
         return reverse("newspapers:newspaper-detail", args=[str(self.id)])
