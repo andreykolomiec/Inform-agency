000965b1104fb0b62b779578e90dbc59afb59599
Andrew <akolomiec982@gmail.com>
2025-03-05 14:14:55 +0200

    update settings.py and requirements.txt

--- inform_agency/urls.py
+++ inform_agency/urls.py
@@ -23,7 +23,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("newspapers/", include("newspapers.urls", namespace="newspapers")),
+    # path("newspapers/", include("newspapers.urls", namespace="newspapers")),
+    path("", include("newspapers.urls", namespace="newspapers")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
